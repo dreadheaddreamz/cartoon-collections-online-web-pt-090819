52c7156b1b0be99c019ea62c66c490559e0d7b9e
James Batsell <jamesbatsell05@gmail.com>
2019-09-18 20:44:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ def long_planeteer_calls(short_words)
 end
 
 def find_the_cheese(cheese)
-  # the array below is here to help
+  cheese.include
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
